91f1f1cc67ca38c00a355dd2f05cb2b7d87dfec6
Kamil Matysik <kamilmatysik@outlook.com>
2026-02-21 15:54:44 +0000

    working on acutally joining the server now

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-function createLobby(){
-    chooseName()
-
+async function createLobby(){
+    if(!await nameCheck()){
+        //let the user know their name is invalid
+        return
+    }
+    showLobbyInputs()
+    document.getElementById("finalCreate").style.display = "block"
 }
-function joinLobby(){
-    chooseName()
+async function joinLobby(){
+    if(!await nameCheck()){
+        //let the user know their name is invalid
+        return
+    }
+    showLobbyInputs()
+    document.getElementById("finalJoin").style.display = "block"
+}
+function nameCheck(){
+    //check if name is not too short or long and also only letters and numbers
+    //if invalid, "return false"
+    return true
+}
+function showLobbyInputs(){
+    document.getElementById("lobbyPass").style.display = "block";
+    document.getElementById("lobbyName").style.display = "block";
 }
 
-function chooseName(){
-    document.getElementById("nameInput").style.opacity = 1;
-    document.getElementById("submitName").style.opacity = 1;
-    //Add checks to ensure name is not too long/too short, only letters and numbers
+async function finalCreate(){
+    if(!await checkLobbyDetails()){
+        //Let user know details need fixing
+        return
+    }
+}
+async function finalJoin(){
+    if(!await checkLobbyDetails()){
+        //Let user know details need fixing
+        return
+    }
+}
+
+
+
+function checkLobbyDetails(){
+    //ensure name is valid (password is optional but if not empty, enusre its valid)
+    //if invalid "return false"
+    return true
 }
